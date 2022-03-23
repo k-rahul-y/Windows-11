26ec457e04004d8ac47efd7ef3759ced9fb59e95
k-rahul-y <94670204+k-rahul-y@users.noreply.github.com>
2022-03-23 12:12:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let win = document.getElementById('win')
+let file = document.getElementById('file')
+let startMenu = document.getElementsByClassName('startMenu')[0]
+let explorer = document.getElementsByClassName('explorer')[0]
+
+
+win.addEventListener("click",()=>{
+    if(startMenu.style.bottom == "60px"){
+        startMenu.style.bottom = "-570px"
+    }
+    else{
+        startMenu.style.bottom = "60px"
+    }
+})
+file.addEventListener("click",()=>{
+    if(explorer.style.bottom == "34px"){
+        explorer.style.bottom = "-700px"
+    }
+    else{
+        explorer.style.bottom = "34px"
+    }
+})
+
+
+
+
+
+
+
